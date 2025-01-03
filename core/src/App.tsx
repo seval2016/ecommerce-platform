39e400262b14db072d795cc754152807b655dda8
seval2016 <sevalsenturk2016@gmail.com>
2025-01-03 22:41:47 +0300

    order list and header design

--- core/src/App.tsx
+++ core/src/App.tsx
@@ -1,33 +1,38 @@
 import React, { Suspense } from "react";
-import "./index.scss";
-import Navigation from "./components/Navigation/Navigation";
+import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+//import UserAuthentication from "userAuthentication/UserAuthentication";
 
 const Products = React.lazy(() => import("productList/Products"));
 const ShoppingCart = React.lazy(() => import("shoppingCart/ShoppingCart"));
 const OrderHistory = React.lazy(() => import("orderHistory/OrderHistory"));
-const UserAuthentication = React.lazy(() => import("userAuthentication/UserAuthentication"));
+const UserAuthentication = React.lazy(
+  () => import("userAuthentication/UserAuthentication")
+);
+
+import "./index.scss";
 
 const App = () => (
   <>
     <Header />
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
-        <Navigation />
         <Routes>
-          <Route path="/products" element={<Products />} />
+          <Route path="/" element={<Products />} />
           <Route path="/auth" element={<UserAuthentication />} />
           <Route path="/cart" element={<ShoppingCart />} />
           <Route path="/orders" element={<OrderHistory />} />
         </Routes>
-
-        <Footer />
       </Router>
     </Suspense>
+    <Footer />
   </>
 );
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
 
-export default App;
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(<App />);
